0ab048d8065748105f7a9f2b0750967a093da83e
Nyandago <paulo@cannybits.com>
2022-05-14 23:48:00 +0300

    startup.cs

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace ScarsMotorGarage
             app.UseStaticFiles();
 
             app.UseRouting();
-
+           
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
